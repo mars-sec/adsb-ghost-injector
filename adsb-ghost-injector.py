e332975f96b33014f7ce9d87ee72dc9bf494082b
mars-sec <marshall.e.yanis@gmail.com>
2026-03-17 22:12:48 -0400

    Added TCP connect and send functions, started ICAO encoding function.

--- adsb-ghost-injector.py
+++ adsb-ghost-injector.py
@@ -1,3 +1,4 @@
+import socket
 
 # CRC-24 function for ADS-B messages
 def crc24(data):
@@ -10,12 +11,54 @@ def crc24(data):
                 crc ^= 0x1FFF409
     return crc & 0xFFFFFF
 
-msg = "8DADB72399955B0DB064A8BADE7D"
 
-data = bytes.fromhex(msg[:22])
-expected_crc = msg[22:]
+# Example of a live ADS-B message
+def test_crc():
+    msg = "8DADB72399955B0DB064A8BADE7D"
 
-result = crc24(data)
-print(f"{result:06X}")
-print(expected_crc)
-input()
+    data = bytes.fromhex(msg[:22])
+    expected_crc = msg[22:]
+
+    result = crc24(data)
+    print(f"Got:            {result:06X}")
+    print(f"Expected:       {expected_crc}")
+    print(f"Match:          {expected_crc.upper() == f'{result:06X}'}")
+
+# TCP connect and send
+def connect(host, port):
+    s = socket.socket()
+    s.settimeout(5)
+
+    try:
+        s.connect((host, port))
+        print(f"[+] Connected to {host}:{port}")
+        return s
+    except Exception as e:
+        print(f"[-] Failed to connect to {host}:{port} - {e}")
+        return None
+
+def send(sock, msg):
+    msgEncoded = f"*{msg};\r\n".encode()
+    sock.sendall(msgEncoded)
+
+# Encode ICAO
+def encode_icao(icao):
+    int(icao, 16)
+    
+
+
+
+# Build messages
+
+# Ghost aircraft class
+
+# main
+if __name__ == "__main__":
+    test_crc()
+
+    sock = connect('127.0.0.1', 30001)
+    if sock:
+        send(sock,"8DADB72399955B0DB064A8BADE7D")
+
+
+    input()
